d7d6e38b6f1e1792abbeb8aba2e0ed27a1a472bd
Nun3s01 <cauanunes77@yahoo.com>
2025-12-18 17:07:33 -0300

    Added responsiveness to the project.

--- src/scripts/lenis-scroll.ts
+++ src/scripts/lenis-scroll.ts
@@ -7,7 +7,6 @@ const lenis = new Lenis({
     smoothWheel: true
 });
 
-// Correção 1: Adicionado o tipo ': number' para o parâmetro time
 function raf(time: number) {
     lenis.raf(time);
     requestAnimationFrame(raf);
@@ -16,19 +15,18 @@ function raf(time: number) {
 requestAnimationFrame(raf);
 
 // Ajuste para âncoras
-// Correção 2: Dizemos ao TS que isso é uma lista de Links (HTMLAnchorElement)
 const anchors = document.querySelectorAll('a[href^="#"]') as NodeListOf<HTMLAnchorElement>;
 
 anchors.forEach((anchor) => {
-    // Usamos Arrow Function '=>' para não ter problemas com o 'this'
     anchor.addEventListener('click', (e) => {
         e.preventDefault();
 
-        // Correção 3: Usamos a própria variável 'anchor' em vez de 'this'
         const targetId = anchor.getAttribute('href');
 
         if (targetId) {
-            lenis.scrollTo(targetId, { offset: -170 });
+            // CORREÇÃO AQUI: Mudamos de -170 para -90 (Altura exata da sua Navbar)
+            // Se quiser uma folga mínima, use -100
+            lenis.scrollTo(targetId, { offset: -70 });
         }
     });
 });
